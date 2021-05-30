7f65c7df0b554129baee3f98c5876c1e49679278
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-05-30 12:47:01 -0300

    Mysql Connection

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { createConnection } from 'mysql2';
 
 const routes = new Router();
+const connection = createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'pass',
+  database: 'banco_aula_workbench',
+});
+connection.connect();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello World',
-  });
+routes.get('/products', (req, res) => {
+  connection.query(
+    'SELECT * FROM banco_aula_workbench.produtos',
+    function (err, rows, fields) {
+      if (err) throw err;
+      res.send(rows);
+    },
+  );
 });
 
 export default routes;
