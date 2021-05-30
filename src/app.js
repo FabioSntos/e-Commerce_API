ea67f45ad895a58a25f0fb043a67bf2d74e092e7
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-05-30 09:55:57 -0300

    package.json fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes.js';
+import routes from './routes';
 
 class App {
   constructor() {
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-import app from './app.js';
+import app from './app';
 
 app.listen(3333);
